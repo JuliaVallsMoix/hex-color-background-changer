63b7b394bbdaa8c5cf117e167c43196dcb1b0b7f
Júlia Valls Moix <juliavalls20@gmail.com>
2023-04-20 14:04:05 +0200

    Done

--- js/app.js
+++ js/app.js
@@ -1,2 +1,21 @@
 // Implementar solución aquí
+function getRandomHexColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+document.querySelector('#btn').addEventListener('click', function() {
+
+    let hexColor = getRandomHexColor();
+
+    document.body.style.backgroundColor = hexColor;
+
+    document.querySelector('#hex-value').innerHTML = " " + hexColor;
+
+})
+
 
